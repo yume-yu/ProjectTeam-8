84463e57c6ab3ad209816e31e37294a07ba08b24
yume_yu <yu.matchang@gmail.com>
2018-04-24 17:46:42 +0900

    win/prog9.cに再び優しさを追加

--- TestCodes/win/prog9.c
+++ TestCodes/win/prog9.c
@@ -17,10 +17,30 @@ int main(){
  	make_flame(WIDTH,HEIGHT,MAIN_FLAME_X_OFFSET,MAIN_FLAME_Y_OFFSET);
 
 	//テスト呼び出し
+	/**
+	  * フレームを作成する
+	  * make_flame(幅,高さ,開始x座標,開始y座標)
+	  */
 	make_flame(3,8,5,7);
+	
+	/**
+	  * 2分割フレームを作成する
+	  * make_vsflame(幅,高さ,開始x座標,開始y座標,分割する位置のx座標)
+	  */
 	make_vsflame(40,5,3,20,5);
-	char string[][100] = {"ほげ","ふが"};
+	
+	
+	/**
+	  * 文字を1行出力する
+	  * print_line(文字列,開始x座標,開始y座標)
+	  */
 	print_line("ほげほげげ",18,21);
+	
+	/**
+	  * 文字を複数行出力する
+	  * print_lines(文字列の配列,開始x座標,開始y座標,行数)
+	  */
+	char string[][100] = {"ほげ","ふが"};
 	print_lines(string,5,21,2);
 	while(!kbhit()){
 	}
