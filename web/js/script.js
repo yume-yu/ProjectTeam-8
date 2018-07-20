f7cd5e1297bab204bb857140fae1f48fc9d7c1f5
yume_yu <yu.matchang@gmail.com>
2018-07-20 20:59:42 +0900

    ディレクトリ構造の整理

---
+++ web/js/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded",function(){
+	document.getElementById("enter_button").addEventListener('click',function(){
+		var elm = document.getElementsByClassName("animation");
+		anime({
+			targets: elm,
+			duration: 3000,
+			opacity: 0,
+			height: 0,
+			complate:function(){
+				elem.style.display="none";
+				elem.style.height="0px";
+			}
+		})
+	})
+})
