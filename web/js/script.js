05a37660c66fdc42827521f24bd016e8e5ba11f0
yume_yu <yu.matchang@gmail.com>
2018-07-21 18:29:22 +0900

    全アニメーションからanime.jsを削除

--- web/js/script.js
+++ web/js/script.js
@@ -1,26 +1,12 @@
-function moveLogo(elem){
-}
-
-document.getElementById("enter_button").addEventListener('click',function(){
-	var elm = document.getElementsByClassName("animation");
-	anime({
-		targets: elm,
-		duration: 2000,
-		opacity: 0,
-		height: 0,
-		complate:function(anim){
-			document.querySelector('.animation').className="afterAnimation"+document.querySelector('animation').className;
-			//document.querySelector("#title_menu").style.display="none";
-		}
-	})
-	document.querySelector('.animation').classList.add("afterAnimation");
+document.querySelector('.animation').addEventListener('click',function(){
+	document.querySelector('.animation').classList.add('started');
 	document.querySelector('body').className = "canScroll";
 	document.querySelector('html').className = "canScroll";
 })
 
 window.addEventListener('load', function(){
 	/* タイトルアニメーション処理 */
-	var title_logo = document.querySelector('h1.animation');
+	var title_logo = document.querySelector('#title_menu h1');
 	title_logo.className="loaded " + title_logo.className;
 })
 
