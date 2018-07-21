d529f2c7c7d57488037a898c4c1a68da0e23a2e7
yume_yu <yu.matchang@gmail.com>
2018-07-21 17:01:47 +0900

    headerをレスポンシブに近づけた

--- web/js/script.js
+++ web/js/script.js
@@ -9,15 +9,17 @@ document.getElementById("enter_button").addEventListener('click',function(){
 		opacity: 0,
 		height: 0,
 		complate:function(anim){
-			document.querySelector('#title_menu').style.zIndex="1";
-			document.querySelector("#title_menu").style.display="none";
+			document.querySelector('.animation').className="afterAnimation"+document.querySelector('animation').className;
+			//document.querySelector("#title_menu").style.display="none";
 		}
 	})
+	document.querySelector('.animation').classList.add("afterAnimation");
 	document.querySelector('body').className = "canScroll";
 	document.querySelector('html').className = "canScroll";
 })
 
 window.addEventListener('load', function(){
+	/* タイトルアニメーション処理 */
 	var title_logo = document.querySelector('h1.animation');
 	title_logo.className="loaded " + title_logo.className;
 })
