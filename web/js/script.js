fc41ec19e63594783babecbf80f90b147edf0d78
yume_yu <yu.matchang@gmail.com>
2018-07-21 15:16:22 +0900

    npmの使用開始とアニメーションの一部をjsに変更

--- web/js/script.js
+++ web/js/script.js
@@ -1,17 +1,24 @@
-document.addEventListener("DOMContentLoaded",function(){
-	document.getElementById("enter_button").addEventListener('click',function(){
-		var elm = document.getElementsByClassName("animation");
-		anime({
-			targets: elm,
-			duration: 3000,
-			opacity: 0,
-			height: 0,
-			complate:function(){
-				elem.style.display="none";
-				elem.style.height="0px";
-			}
-		})
-		document.querySelector('body').className = "canScroll";
-		document.querySelector('html').className = "canScroll";
+function moveLogo(elem){
+}
+
+document.getElementById("enter_button").addEventListener('click',function(){
+	var elm = document.getElementsByClassName("animation");
+	anime({
+		targets: elm,
+		duration: 2000,
+		opacity: 0,
+		height: 0,
+		complate:function(anim){
+			document.querySelector('#title_menu').style.zIndex="1";
+			document.querySelector("#title_menu").style.display="none";
+		}
 	})
+	document.querySelector('body').className = "canScroll";
+	document.querySelector('html').className = "canScroll";
 })
+
+window.addEventListener('load', function(){
+	var title_logo = document.querySelector('h1.animation');
+	title_logo.className="loaded " + title_logo.className;
+})
+
