87ec6cdeee76ec6ce62f7301942d82085c5f789b
yume_yu <yu.matchang@gmail.com>
2018-07-23 02:07:45 +0900

    キャラ紹介を仮あてで実装

--- web/js/script.js
+++ web/js/script.js
@@ -4,12 +4,34 @@ document.querySelector('.animation').addEventListener('click',function(){
 	document.querySelector('html').className = "canScroll";
 })
 
-for(var i = 0; i < document.getElementById('Character').getElementsByTagName('button').length; i++){
-	document.getElementById('Character').getElementsByTagName('button')[i].addEventListener('click',function(){
-		document.getElementById('Character').querySelector('.selected').classList.toggle('selected');
-		event.target.classList.add('selected');
-	})
+function switchExp(label){
+	document.getElementById(label).classList.toggle('selected');
 }
+
+document.getElementById('Character').getElementsByTagName('button')[0].addEventListener('click',function(){
+	document.getElementById('Character').querySelector('.selected').classList.toggle('selected');
+	event.target.classList.add('selected');
+	for(var i = 0; i < document.getElementById('Character').getElementsByTagName('div').length; i++){
+		document.getElementById('Character').getElementsByTagName('div')[i].classList.remove('selected');
+	}
+	switchExp('arist_exp');
+})
+document.getElementById('Character').getElementsByTagName('button')[1].addEventListener('click',function(){
+	document.getElementById('Character').querySelector('.selected').classList.toggle('selected');
+	event.target.classList.add('selected');
+	for(var i = 0; i < document.getElementById('Character').getElementsByTagName('div').length; i++){
+		document.getElementById('Character').getElementsByTagName('div')[i].classList.remove('selected');
+	}
+	switchExp('naoki_exp');
+})
+document.getElementById('Character').getElementsByTagName('button')[2].addEventListener('click',function(){
+	document.getElementById('Character').querySelector('.selected').classList.toggle('selected');
+	event.target.classList.add('selected');
+	for(var i = 0; i < document.getElementById('Character').getElementsByTagName('div').length; i++){
+		document.getElementById('Character').getElementsByTagName('div')[i].classList.remove('selected');
+	}
+	switchExp('lirel_exp');
+})
 
 window.addEventListener('load', function(){
 	window.scroll(0,0);
