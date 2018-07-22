1ff83915523083dfce858fafcab8c17d7212cbb0
yume_yu <yu.matchang@gmail.com>
2018-07-23 00:56:37 +0900

    キャラクター紹介の準備

--- web/js/script.js
+++ web/js/script.js
@@ -4,6 +4,13 @@ document.querySelector('.animation').addEventListener('click',function(){
 	document.querySelector('html').className = "canScroll";
 })
 
+for(var i = 0; i < document.getElementById('Character').getElementsByTagName('button').length; i++){
+	document.getElementById('Character').getElementsByTagName('button')[i].addEventListener('click',function(){
+		document.getElementById('Character').querySelector('.selected').classList.toggle('selected');
+		event.target.classList.add('selected');
+	})
+}
+
 window.addEventListener('load', function(){
 	window.scroll(0,0);
 	/* タイトルアニメーション処理 */
