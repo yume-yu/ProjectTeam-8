aea166c7fe1ff945950054226f2502f8b3899a87
yume_yu <yu.matchang@gmail.com>
2018-07-22 23:22:46 +0900

    再読込時に最上部へスクロールするように変更

--- web/js/script.js
+++ web/js/script.js
@@ -5,6 +5,7 @@ document.querySelector('.animation').addEventListener('click',function(){
 })
 
 window.addEventListener('load', function(){
+	window.scroll(0,0);
 	/* タイトルアニメーション処理 */
 	var title_logo = document.querySelector('#title_menu h1');
 	title_logo.className="loaded " + title_logo.className;
